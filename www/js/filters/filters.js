3a7dbb7b5a479ff512ec3c77cf9efe84dc6f530e
David F. Herrera <davidfherrerar@gmail.com>
2015-09-15 15:21:07 -0400

    Fix Notifications View
    New filter to provide an IOS email like date system for the
    notifications, fixed the unread to, no more go to individual
    notifications, now it directs to the page where the notification comes
    from just like Facebook. Fix the notification read error, fix the SQL
    tables

--- www/js/filters/filters.js
+++ www/js/filters/filters.js
@@ -73,7 +73,32 @@ var myApp=angular.module('MUHCApp.filters',[]);
         }
       }
 	});
+myApp.filter('dateEmail',function($filter){
+  return function(date){
+    var day=date.getDate();
+    var month=date.getMonth();
+    var year=date.getFullYear();
+    var newDate=new Date();
+    if(newDate.getMonth()==month&&newDate.getFullYear()==year)
+    {
+      if(day==newDate.getDate())
+      {
+        return $filter('date')(date, 'h:mm a');
+      }else if(day-newDate.getDate()==1){
+        return 'Yesterday';
+      }else{
+        return $filter('date')(date, 'dd/MM/yyyy'); 
+      }
+    }else{
+      return $filter('date')(date, 'dd/MM/yyyy'); 
+    }
 
+
+
+
+  };
+
+});
 myApp.filter('propsFilter', function() {
   return function(items, props) {
     var out = [];
