bb57a76d7d8e10fd2d237f56bd5fdb6b0002ae3f
David Herrera <davidfherrerar@gmail.com>
2015-09-26 13:55:08 -0400

    Educational Material Page update to give link to view on planning for your radiotherapy and access to the radiotherapy booklet at the cedars cancer centre

--- www/js/controllers/EducationalMaterialController.js
+++ www/js/controllers/EducationalMaterialController.js
@@ -5,7 +5,13 @@ $scope.openPDF=function(){
 	var app = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
 	//file:///data/data/com.example.hello/files/pdfs
 	var ref = window.open('./pdfs/radiotherapy_journey.pdf', '_blank', 'location=yes');
-}
+};
+
+$scope.openVideo=function(){
+
+    var ref = window.open('https://www.youtube.com/watch?v=2dPfuxb1H8E', '_blank', 'location=yes');
+
+};
 /*var options = {
     date: new Date(),
     mode: 'date', // or 'time'
